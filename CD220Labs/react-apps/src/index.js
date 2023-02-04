3294ee528caf558806d329e54cd5cbb17c7cc22d
Dennis <Dennismalayil93@yahoo.com>
2023-02-03 21:48:22 -0600

    added comments to index.js

--- CD220Labs/react-apps/src/index.js
+++ CD220Labs/react-apps/src/index.js
@@ -1,10 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
 
-ReactDOM.render(
+
+import React from 'react';    //The first two lines import the React and ReactDOM libraries, which are the core libraries for building React applications.
+import ReactDOM from 'react-dom';
+import App from './App';    //This line imports the "App" component, which is defined in a separate file called "App.js".
+
+ReactDOM.render(      //This function from the ReactDOM library is used to render a React component into a DOM element. The first argument to render is the component to be rendered, and the second argument is the DOM element that it should be rendered into.
   <React.StrictMode>
-    <App />
+    <App color="green" size="20" />
   </React.StrictMode>,
-  document.getElementById('root')
+  //This is the "App" component being rendered. The color and size properties are passed as props to the component. 
+  //This is a special component that enables strict mode for React, which is a development-only feature that adds checks for potential problems in your code.
+  document.getElementById('root')   //This is the DOM element with an id of "root", into which the "App" component will be rendered.
 );
